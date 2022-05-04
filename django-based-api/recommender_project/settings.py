fa651c22c2b292c07023e4fc483f7d7abd5fd0b9
MohamedBen <95242726+mohbenaicha@users.noreply.github.com>
2022-05-03 21:13:30 -0400

    added custom logger

--- django-based-api/recommender_project/settings.py
+++ django-based-api/recommender_project/settings.py
@@ -124,3 +124,22 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+            'formatter': 'custom_1'
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'DEBUG',
+    },
+    'formatters': {
+        'custom_1': {'format': '| {levelname}  | {message}', 'style': '{'}
+    }
+}
